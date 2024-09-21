dae56d8b8b7b06f4098f479863cadbaeed1db5fa
Vladimir Shumkov <augusthoffman3@gmail.com>
2024-09-21 14:12:05 +0500

    create the first game EVEN and add asciinema to README

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String userChoice = scanner.next();
         System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        switch (userChoice) {
+            case "1":
+                Cli.greeting();
+                break;
+            case "2":
+                Cli.greeting();
+                Even.evenGame();
+                break;
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    static String userName;
+
     public static void greeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
-        String userName = scanner.next();
+        userName = scanner.next();
         System.out.println("Hello, " + userName);
     }
 }
